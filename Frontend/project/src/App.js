87df50cd61f64391575426002d2f45bf191fb4ce
unknown <aniketghormare62@gmail.com>
2023-06-23 10:03:40 +0530

    aniket commit

---
+++ Frontend/project/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+
+function App() {
+  return (
+    <div >
+     {/* <Login/> */}
+     <Signup/>
+    </div>
+  );
+}
+
+export default App;
